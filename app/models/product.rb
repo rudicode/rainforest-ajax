561f239c92f6a6502785b4730d1d597d1340ed26
Rudi Luzar <rudi@rudicode.com>
2014-11-09 09:08:41 -0500

    Now displaying price in dollars, added method to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :description, :name, presence: true
+  validates :price_in_cents, numericality: { only_integer: true}
+
+  def price_in_dollars
+    dollars = price_in_cents.to_f / 100.0
+    dollars = sprintf("$%.2f", dollars)
+  end
 end

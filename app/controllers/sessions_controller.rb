5ac01fc0eb283f04520434fde1b82e02e8e3c38f
Rudi Luzar <rudi@rudicode.com>
2014-11-09 10:58:38 -0500

    Added notices and flash messages to application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
     logger.info "[Rainforest.SessionsController.create] Trying to log in #{params[:email]}"
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to products_url, notice: "#{user.email}Logged In."
+      redirect_to products_url, notice: "#{user.email} Logged In."
     else
+      flash.now[:alert] = "Invalid email or password"
       render :new
     end
   end

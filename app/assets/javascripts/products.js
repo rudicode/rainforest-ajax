a61db21db558691cb09c40e38999a553eb2df551
Rudi Luzar <rudi@rudicode.com>
2014-11-28 10:31:08 -0500

    Product#index now loads as many pages as necessary to fill the page. So that the scrollbars activate and we can use infinate scroll without zooming the page.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,11 +1,39 @@
 // products.js
 $(document).on('ready page:load',function(){
+
+  // my js
+  function pageNotFilled () {
+    return $(window).height() > $('body').height();
+  }
+
   $('#products_list').text('The Product Listing'); // change the header
+
+  // only if displaying the products ist.
+  if ( $('#products_list').length > 0) {
+
+    var finishedLoading = false;
+
+    while ( pageNotFilled() ){
+        // console.log('Page not filled');
+        // load next page of data, same as the scrolling event below.
+      var url = $('.pagination span.next').children().attr('href');
+      console.log(url);
+      // $('.pagination').text('Getting more results...');
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'script',
+        async: false
+      });
+
+    }
+  }
+
   $('#search-form').submit( function (event){
     event.preventDefault();
     var searchValue = $('#search').val();
 
-    $.getScript('/products?search=' + searchValue + "&test=testing")
+    $.getScript('/products?search=' + searchValue )
     
   });
 
